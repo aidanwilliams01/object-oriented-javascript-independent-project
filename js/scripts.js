198b7e77b90ea79f25fd5212a559f5b59c82de2a
Aidan Williams <aidanwilliams01@gmail.com>
2023-06-08 15:27:11 -0700

    add functionality to allow the user to add multiple pizzas to their order

--- js/scripts.js
+++ js/scripts.js
@@ -21,21 +21,23 @@ function Order(currentPrice) {
   this.currentPrice = currentPrice;
 }
 
-
 Order.prototype.calculatePrice = function(pizzaPrice) {
   this.currentPrice += pizzaPrice;
   return this.currentPrice;
 }
 
-function handleFormSubmission(event) {
-  event.preventDefault();
-  const size = document.querySelector("input[name='size']:checked").value;
-  const toppings = document.querySelectorAll("input[name='toppings']:checked");
-  const myPizza = new Pizza(toppings, size);
-  document.querySelector("p").innerText = `Price: $${myPizza.calculatePrice()}`;
+function onPageLoad() {
+  const form = document.querySelector("form");
+  const myOrder = new Order(0);
+  function formSubmission(event) {
+    event.preventDefault();
+    const size = document.querySelector("input[name='size']:checked").value;
+    const toppings = document.querySelectorAll("input[name='toppings']:checked");
+    const myPizza = new Pizza(toppings, size);
+    const pizzaPrice = myPizza.calculatePrice();
+    document.querySelector("p").innerText = `Price: $${myOrder.calculatePrice(pizzaPrice)}`;
+  }
+  form.addEventListener("submit", formSubmission);
 }
 
-window.addEventListener("load", function () {
-  const myOrder = Order(0);
-  this.document.querySelector("form").addEventListener("submit", handleFormSubmission);
-});
+window.addEventListener("load", onPageLoad);
