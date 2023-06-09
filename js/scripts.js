f604d4852151723f6a49fb186ba3491af64a19ff
Aidan Williams <aidanwilliams01@gmail.com>
2023-06-09 15:13:39 -0700

    update functionality to display pizzas ordered

--- js/scripts.js
+++ js/scripts.js
@@ -17,29 +17,38 @@ Pizza.prototype.calculatePrice = function() {
   return price;
 }
 
-function Order(currentPrice) {
+function Order(currentPrice, pizzas) {
   this.currentPrice = currentPrice;
+  this.pizzas = pizzas;
 }
 
 Order.prototype.calculatePrice = function(pizzaPrice) {
   this.currentPrice += pizzaPrice;
+  this.pizzas += 1;
   return this.currentPrice;
 }
 
 function onPageLoad() {
   const form = document.querySelector("form");
-  const myOrder = new Order(0);
+  const myOrder = new Order(0, 0);
   function formSubmission(event) {
     event.preventDefault();
     const size = document.querySelector("input[name='size']:checked").value;
     const toppings = document.querySelectorAll("input[name='toppings']:checked");
     const myPizza = new Pizza(toppings, size);
     const pizzaPrice = myPizza.calculatePrice();
+    const brElement = document.createElement("br");
+    const brElement2 = document.createElement("br");
+    const brElement3 = document.createElement("br");
+    document.querySelector("p#price").innerText = `Price: $${myOrder.calculatePrice(pizzaPrice)}`;
+    document.querySelector("p#pizzas").append(`Pizza ${myOrder.pizzas}`);
+    document.querySelector("p#pizzas").append(brElement);
     myPizza.toppings.forEach(topping => {
       document.querySelector("p#pizzas").append(`${topping.value}, `);
     });
     document.querySelector("p#pizzas").append(`${myPizza.size}, $${pizzaPrice}`);
-    document.querySelector("p#price").innerText = `Price: $${myOrder.calculatePrice(pizzaPrice)}`;
+    document.querySelector("p#pizzas").append(brElement2);
+    document.querySelector("p#pizzas").append(brElement3);
   }
   form.addEventListener("submit", formSubmission);
 }
